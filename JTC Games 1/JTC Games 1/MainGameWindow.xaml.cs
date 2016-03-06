bf78884c2b4b3bd1fe779388c424977bed88a2a8
LauchyTheProgrammer <lachyisyum@hotmail.com>
2016-03-06 17:26:26 +1100

    Modifications to MainDeckWindow!
    lol

--- JTC Games 1/JTC Games 1/MainGameWindow.xaml.cs
+++ JTC Games 1/JTC Games 1/MainGameWindow.xaml.cs
@@ -26,8 +26,26 @@ namespace JTC_Games_1
 
         private void Deck_Click(object sender, RoutedEventArgs e)
         {
-            MainDeckWindow deckWindow = new MainDeckWindow();
-            deckWindow.Show();
+            //Testing if it's false each time...
+            bool isWindowOpen = false;
+
+            //For each Window in the current list of Windows then...
+            foreach (Window theWindow in Application.Current.Windows)
+            {
+                //...if the variable theWindow is the MainDeckWindow window then...
+                if (theWindow is MainDeckWindow)
+                {
+                    isWindowOpen = true;
+                    theWindow.Activate();
+                }
+            }
+            //MainDeckWindow window doesn't exist!
+            //Make one...
+            if (!isWindowOpen)
+            {
+                MainDeckWindow newWindow = new MainDeckWindow();
+                newWindow.Show();
+            }
         }
     }
 }
