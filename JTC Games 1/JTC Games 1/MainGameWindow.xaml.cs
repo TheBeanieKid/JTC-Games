bceec49ad736e2dba040c6e6855eae363be8f08c
LauchyTheProgrammer <lachyisyum@hotmail.com>
2016-03-08 18:55:57 +1100

    Things u wont understand!
    u wont understand

--- JTC Games 1/JTC Games 1/MainGameWindow.xaml.cs
+++ JTC Games 1/JTC Games 1/MainGameWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace JTC_Games_1
 {
@@ -19,13 +20,39 @@ namespace JTC_Games_1
     /// </summary>
     public partial class MainGameWindow : Window
     {
-
+        //Make a new timer to update every so, 1 tenth of a sec?
+        DispatcherTimer t = new DispatcherTimer();
         public MainGameWindow()
         {
             InitializeComponent();
-
+            t.Interval = new TimeSpan(0,0,0,0,1);
+            t.Tick += new EventHandler(timersTick);
+            t.Start();
             LumberCampDetails.Text = " Details :\n\n Bulid Cost :   40 Wood\n\n Production/s :   1";
             LumberCampDetails.Visibility = Visibility.Collapsed;
+        }
+
+        //Will do this method every 1 millisecond lol 
+        private void timersTick(object sender, EventArgs e)
+        {
+            //gets mouse's position, then u can check for its X pos by doing point.X, so on
+            var point = Mouse.GetPosition(this);
+            //So the mouse is in the middle of the rectangle when it follows V
+            Canvas.SetTop(LumberCamp, point.Y - (LumberCamp.Height / 2));
+            Canvas.SetLeft(LumberCamp, point.X - (LumberCamp.Width / 2));
+
+            //For rectangle collisions, you need to make a clone but make it a 'rect'
+            Rect rectPlayersObject = new Rect(Canvas.GetLeft(LumberCamp), Canvas.GetTop(LumberCamp), LumberCamp.Width, LumberCamp.Height);
+            //Whatever has to be a rect so do this instead change LumberCamp to Whatever or so what ^
+            if (rectPlayersObject.IntersectsWith(Whatever) == true)
+            {
+                //It'll do whatever if the rectangle intersects with Whatever
+            }
+            //For mouse collisions
+            if (Mouse.DirectlyOver == LumberCamp)
+            {
+                //If mouse is hovering over the LumberCamp then do shit in here
+            }
         }
 
         private void LumberCamp_MouseEnter(object sender, MouseEventArgs e)
@@ -45,7 +72,7 @@ namespace JTC_Games_1
 
         private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            Rect_1.Fill = new SolidColorBrush(Colors.Red);
+
         }
     }
 }
